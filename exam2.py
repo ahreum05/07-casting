b4c061d5ed82f3fa2cb6c65b6efe21e5171dc5b0
ahreum <ahahreum19990@naver.com>
2024-10-08 03:20:32 +0900

    first commit

---
+++ exam2.py
@@ -0,0 +1,20 @@
+a = 1
+b = 0
+print(bool(a), bool(b))
+print('-' * 20)
+
+s1 = "abc"
+s2 = ""
+print(bool(s1), bool(s2))
+print('-' * 20)
+
+print(bool(None))
+print('-' * 20)
+
+# 레퍼런스 변수만 만들고 싶을 때, None 사용
+xx = None
+print(xx)
+print('-' * 20)
+
+print(bool(xx))
+print('-' * 20)
